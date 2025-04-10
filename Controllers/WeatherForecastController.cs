f319364afc1fd99ef4eb2064750d139a263e91ba
Hasan Emre AÇIKAY <hasan.acikay@ibb.gov.tr>
2025-04-10 08:33:28 +0300

    update check

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace Voltance_proxy_api.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "GetWeatherForecast")] 
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
